31b63cb7568068f891529faeba4d9e335fa6a7e3
filipe-wesley <filipewesley06@gmail.com>
2025-11-25 17:11:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// Detectando clique do mouse nos botões da bateria.
+
+// Laço que percorre todos os botões da bateria.
+
+// Adicionar um evento a cada botão para o evento de "click".
+    
+// Detectar o pressionamento de teclas no teclado.
+
+// Função que toca o som correspondente a uma tecla (pesquisar sobre o case).
+
+// Função que anima o botão que foi pressionado ou clicado.
